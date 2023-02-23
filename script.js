cef8a487b8e707d4c3d27f0528b0ecb6c60b6bc5
AtelierCT6 <atelier.ct6@gmail.com>
2023-02-23 15:56:50 +0100

    pics webp

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let container = document.querySelector(".banner")
 let imgBox = document.querySelector(".img_box");
-let left_btn = document.querySelector(".left_btn");
-let right_btn = document.querySelector(".right_btn");
+// let left_btn = document.querySelector(".left_btn");
+// let right_btn = document.querySelector(".right_btn");
 
 let index = 0;
 let timer;
@@ -9,31 +9,7 @@ let timer;
 timer = setInterval(run, 1500);
 
 function run(){
-    index++;
     if (index >= 4) {
-        index = 4;
-        imgBox.style.transition = 'all linear 1.5s';
-        imgBox.style.left = -index * 300 + 'px';
-        setTimeout(()=> {
-            index = 0;
-            imgBox.style.transition = 'none';
-            imgBox.style.left = -index * 300 + 'px';
-        }, 1500)
-    } else {
-        imgBox.style.transition = 'all linear 1.5s';
-        imgBox.style.left = -index * 300 + 'px';
-    }
-}
-
-container.onmouseover = function() {
-    clearInterval(timer);
-}
-container.onmouseout = function() {
-    timer = setInterval(run, 1500); 
-}
-
-right_btn.addEventListener('click', () => {
-    if (index >= 3) {
         index = 4;
         imgBox.style.transition = 'all linear 1.5s';
         imgBox.style.left = -index * 300 + 'px';
@@ -47,21 +23,45 @@ right_btn.addEventListener('click', () => {
         imgBox.style.transition = 'all linear 1.5s';
         imgBox.style.left = -index * 300 + 'px';
     }
-});
+}
 
-left_btn.addEventListener('click', ()=>{
-    if (index <= 0) {
-        index = 0;
-        imgBox.style.transition = 'all linear 1.5s';
-        imgBox.style.left = 300 + 'px';     
-        setTimeout(function() {
-            index = 3;
-            imgBox.style.transition = 'none';
-            imgBox.style.left = -index * 300 + 'px';
-        }, 1500);
-    } else {
-        index--;
-        imgBox.style.transition = 'all linear 1.5s';
-        imgBox.style.left = -index * 300 + 'px';
-    }
-});
+container.onmouseover = function() {
+    clearInterval(timer);
+}
+container.onmouseout = function() {
+    timer = setInterval(run, 1500); 
+}
+
+// right_btn.addEventListener('click', () => {
+//     if (index >= 3) {
+//         index = 4;
+//         imgBox.style.transition = 'all linear 1.5s';
+//         imgBox.style.left = -index * 300 + 'px';
+//         setTimeout(()=> {
+//             index = 0;
+//             imgBox.style.transition = 'none';
+//             imgBox.style.left = -index * 300 + 'px';
+//         }, 1500)
+//     } else {
+//         index++;
+//         imgBox.style.transition = 'all linear 1.5s';
+//         imgBox.style.left = -index * 300 + 'px';
+//     }
+// });
+
+// left_btn.addEventListener('click', ()=>{
+//     if (index <= 0) {
+//         index = 0;
+//         imgBox.style.transition = 'all linear 1.5s';
+//         imgBox.style.left = 300 + 'px';     
+//         setTimeout(function() {
+//             index = 3;
+//             imgBox.style.transition = 'none';
+//             imgBox.style.left = -index * 300 + 'px';
+//         }, 1500);
+//     } else {
+//         index--;
+//         imgBox.style.transition = 'all linear 1.5s';
+//         imgBox.style.left = -index * 300 + 'px';
+//     }
+// });
